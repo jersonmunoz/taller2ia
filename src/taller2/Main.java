a43ccbf081c72da13645049b472b55a652a3c514
Jerson <jerson.munoz@alumnos.ucn.cl>
2020-11-19 23:08:36 -0300

    Primer commit

---
+++ src/taller2/Main.java
@@ -0,0 +1,13 @@
+package taller2;
+ import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		 System.out.print("Enter text: ");
+	     String myString = input.next();
+		System.out.println("Texto: "+myString);
+		
+	}
+
+}
